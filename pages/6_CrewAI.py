80caf028f297130060bfa6ff48dab8685cef176d
Samit Watve <72366011+samitwatve@users.noreply.github.com>
2024-04-03 05:25:34 +0000

    some changes

--- pages/6_CrewAI.py
+++ pages/6_CrewAI.py
@@ -1,17 +1,25 @@
 from crewai import Agent
-from crewai_tools import SerperDevTool
-search_tool = SerperDevTool()
 import streamlit as st
 from langchain_groq import ChatGroq
 from crewai import Task
 from crewai import Crew, Process
 from contextlib import redirect_stdout
 import io
+import streamlit as st
+
+
 
 openai_api_key = st.secrets["OPEN_AI_API_KEY"]
 groq_api_key = st.secrets["GROQ_API_KEY"]
 serp_api_key = st.secrets["SERP_API_KEY"]
 groq_models = ["llama2-70b-4096", "mixtral-8x7b-32768", "gemma-7b-it"]
+
+
+
+
+
+from crewai_tools import WebsiteSearchTool
+WST = WebsiteSearchTool()
 
 llm = ChatGroq(
             temperature=0.7, 
@@ -31,7 +39,7 @@ researcher = Agent(
     "innovation, eager to explore and share knowledge that could change"
     "the world."
   ),
-  tools=[search_tool],
+  tools=[WST],
   allow_delegation=True,
   llm = llm
 )
@@ -47,7 +55,7 @@ writer = Agent(
     "engaging narratives that captivate and educate, bringing new"
     "discoveries to light in an accessible manner."
   ),
-  tools=[search_tool],
+  tools=[WST],
   allow_delegation=False,
   llm = llm
 )
@@ -63,7 +71,7 @@ research_task = Task(
     "its market opportunities, and potential risks."
   ),
   expected_output='A comprehensive 3 paragraphs long report on the latest AI trends.',
-  tools=[search_tool],
+  tools=[WST],
   agent=researcher,
 )
 
@@ -75,7 +83,7 @@ write_task = Task(
     "This article should be easy to understand, engaging, and positive."
   ),
   expected_output='A 4 paragraph article on {topic} advancements formatted as markdown.',
-  tools=[search_tool],
+  tools=[WST],
   agent=writer,
   async_execution=False,
   output_file='new-blog-post.md'  # Example of output customization
@@ -91,34 +99,20 @@ crew = Crew(
   verbose=True
 )
 
-import logging
-import io
 
-# Create a stream for logging
-log_stream = io.StringIO()
-handler = logging.StreamHandler(log_stream)
-
-# Get the CrewAI logger and add the handler
-logger = logging.getLogger('crewai')
-logger.setLevel(logging.INFO)  # Adjust as necessary for the verbosity you want
-logger.addHandler(handler)
-
-def get_agent_thoughts():
-    # Clear previous log stream content
-    log_stream.seek(0)
-    log_stream.truncate()
-
-    # Run the agent's task
-    # e.g., result = crew.kickoff(inputs={'some_input': 'value'})
-    # This is where you would run the actual Crew AI operation
-
-    # Return the contents of the log stream
-    return log_stream.getvalue()
-
-
+# class CapturingList(list):
+#     def __enter__(self):
+#         self._stdout = sys.stdout
+#         sys.stdout = self._stringio = StringIO()
+#         return self
+#     def __exit__(self, *args):
+#         self.extend(self._stringio.getvalue().splitlines())
+#         del self._stringio    # free up some memory
+#         sys.stdout = self._stdout
 
 if topic := st.chat_input("What would you like to research?"):
     # Starting the task execution process with enhanced feedback
     result = crew.kickoff(inputs={'topic': topic})
-    thoughts = get_agent_thoughts()
-    st.text_area("Agent's Thought Process:", thoughts, height=300)
+    st.text_area("Agent's Thought Process:", result, height=300)
+
+
